422432e66ef2f179926e8f74f81b09a6f85dc47b
Valentina <yunnicova@gmail.com>
2026-03-26 16:58:01 +0100

    fix: add Field.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-//! списки и метод mep учимся рендерить массивы в разметке
-// пробегаемся по массиву mep и возвращаем новый массив
+import Field from "./components/Field";
+
 const App = () => {
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
       <form className="todo__form">
-        <div className="todo__field field">
-          <label className="field__label" htmlFor="new-task">
-            New task
-          </label>
-          <input className="field__input" id="new-task" placeholder=" " autoComplete="off" />
-        </div>
+    <Field />
+        
         <button className="button" type="submit">
           Add
         </button>
@@ -68,6 +64,29 @@ const App = () => {
           <input className="todo-item__checkbox" id="task-2" type="checkbox" />
           <label className="todo-item__label" htmlFor="task-2">
             Task 2
+          </label>
+          <button className="todo-item__delete-button" aria-label="Delete" title="Delete">
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15 5L5 15M5 5L15 15"
+                stroke="#757575"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+        </li>
+        <li className="todo__item todo-item">
+          <input className="todo-item__checkbox" id="task-2" type="checkbox" />
+          <label className="todo-item__label" htmlFor="task-2">
+            Task 3
           </label>
           <button className="todo-item__delete-button" aria-label="Delete" title="Delete">
             <svg
